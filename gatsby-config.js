d55b5016b8b3569485370d419227480dfdd8c859
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-10-23 08:51:17 -0700

    config: add gatsby packages to transform md files to react pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,18 +7,20 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'gatsby-starter-default',
+        name: 'pages',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+        path:`${__dirname}/src/pages`,
       },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    'gatsby-transformer-remark'
   ],
 }
