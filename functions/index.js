4d48b00953f1ca8334cad89c62c21cc656261477
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-11-08 19:57:54 -0800

    contact form: update auto email message

--- functions/index.js
+++ functions/index.js
@@ -10,21 +10,34 @@ exports.sendEmailNotification = functions.firestore.document('contacts/{id}')
   .onCreate((snap, ctx) => {
     const data = snap.data();
     let transporter = nodemailer.createTransport({
-      service:'gmail',
-      host:'smtp.gmail.com',
-      port:456,
-      secure:true,
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 456,
+      secure: true,
       auth: {
         user: SENDER_EMAIL,
         pass: SENDER_PASSWORD
       }
-    }) 
+    })
     transporter.sendMail({
       from: SENDER_EMAIL,
       to: `${data.email}`,
-      subject: 'Your submission Info',
-      text: `${data.email}`,
-      html: `${data.email}`,
+      bcc: SENDER_EMAIL,
+      subject: `Work with Kelly Gowing`,
+      html: `
+      <h2>Hello ${data.name}!</h2>
+      <br>
+      <p> Thanks for your message! I will get back to you at ${data.email} as soon as I can. In the meantime, If you have any updates on your project you can reply to this email.</p>
+      <br>
+      <p>---</p>
+      <h3>Your message:</h3>
+      <p>${data.message}</p>
+      <p>---</p>
+      <br>
+      <p>I look forward to working with you!</p>
+      <br>
+      <p>Kelly Gowing</p>
+      `,
     }).then(res => console.log('successfully sent that mail')).catch(err => console.log(err));
 
   });
