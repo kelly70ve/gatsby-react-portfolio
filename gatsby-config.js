2c8dc9af18e2ac54906f14c67b355a59b83c25bf
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-11-15 11:53:19 -0800

    config: md inline gifs ref:https://www.gatsbyjs.com/plugins/gatsby-remark-copy-linked-files/

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-        path:`${__dirname}/src/pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     {
@@ -26,28 +26,34 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        `gatsby-remark-relative-images`,
-        {
-          resolve: `gatsby-remark-images`,
-          options: {
-            // It's important to specify the maxWidth (in pixels) of
-            // the content container as this plugin uses this as the
-            // base for generating different widths of each image.
-            maxWidth: 800,
-            linkImagesToOriginal: false,
-            sizeByPixelDensity: true,
-            showCaptions: true
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-relative-images`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 800,
+              linkImagesToOriginal: false,
+              sizeByPixelDensity: true,
+              showCaptions: true
+            },
           },
-        },
-      ],
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+        ],
+      },
     },
-  },
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
