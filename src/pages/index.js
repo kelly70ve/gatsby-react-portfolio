138172121de7c0f7df071e0aba500a10396dab7c
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-12-10 20:01:35 -0800

    main: add technical skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,8 @@ const HomeIndex = () => {
       <Helmet>
         <title>{siteTitle}</title>
         <meta name="description" content={siteDescription} />
+        {/* fontawesome */}
+        <script src="https://kit.fontawesome.com/8dda7d33fe.js" crossorigin="anonymous"></script>
         <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
       </Helmet>
 
@@ -21,12 +23,28 @@ const HomeIndex = () => {
         <section id="one">
           <header className="major">
             <h2>
-            Hi, I'm Kelly Gowing & I'm a front end web-developer
+            Hi, I'm Kelly Gowing
             </h2>
           </header>
           <p>
-          2020 UCSD Full-Stack Web Development Bootcamp alumni and 2019 SDSU Marketing graduate. In December of 2019, I decided to make a major career change from Event Planning to Full Stack Web Development. My dream is to be a UI/UX developer or Product Manager, where I will utilize my skills in business, design, and development.
+          Full Stack Web Developer with a background in Marketing and Event Management. I'm a 2020 UCSD Full-Stack Web Development Bootcamp and 2019 SDSU Marketing graduate. My dream is to be a UI/UX developer or Product Manager.
           </p>
+          <h3>Technical Skills:</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td><i className="skills fab fa-3x fa-html5"><span className="tooltiptext">HTML</span></i></td>
+                <td><i className="skills fab fa-3x	fa-css3-alt"><span className="tooltiptext">CSS</span></i></td>
+                <td><i className="skills fab fa-3x	fa-react"><span className="tooltiptext">React</span></i></td>
+                <td><i className="skills fab fa-3x	fa-js-square"><span className="tooltiptext">JavaScript</span></i></td>
+                <td><i className="skills fab fa-3x	fa-node-js"><span className="tooltiptext">Node.js</span></i></td>
+                <td><i className="skills fab fa-3x	fa-git"><span className="tooltiptext">Git</span></i></td>
+                <td><i className="skills fab fa-3x	fa-shopify"><span className="tooltiptext">Liquid(Shopify)</span></i></td>
+                <td><i className="skills fab fa-3x	fa-bootstrap"><span className="tooltiptext">Bootstrap</span></i></td>
+                <td><i className="skills fas fa-3x fa-database"><span className="tooltiptext">SQL</span></i></td>
+              </tr>
+            </tbody>
+          </table>
           <ul className="actions">
             <li>
               <a href="#worktogether" className="button">
