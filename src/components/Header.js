e681292d96945dc2ec2b814705b1a991e4e55c65
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-12-10 22:34:58 -0800

    header: fix sizing issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,26 @@ import React from 'react'
 import Footer from './Footer'
 import avatar from '../assets/images/avatar.png'
 
+const style = {
+  heading: {
+    display: "flex",
+    flexDirection:"column"
+  },
+  links: {
+    borderBottom: "none"
+  }
+}
+
 const Header = () => (
   <header id="header">
     <div className="inner">
-      <a href="#" className="image avatar">
+      <a href="/" className="image avatar">
         <img src={avatar} alt="Kelly Gowing" />
       </a>
-      <h1>
+      <h1 style={style.heading}>
         <strong>Kelly Gowing</strong>
-        <br />
-        <a href="mailto:kelly.a.gowing@gmail.com">kelly.a.gowing@gmail.com</a>
-        <br />
-        <a href="tel:619-655-6803">(619)-655-6803</a>
+        <a style={style.links} href="mailto:kelly.a.gowing@gmail.com">kelly.a.gowing@gmail.com</a>
+        <a style={style.links} href="tel:619-655-6803">(619)-655-6803</a>
       </h1>
     </div>
     <Footer />
