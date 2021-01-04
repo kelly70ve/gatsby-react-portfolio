ea00faf417b416f50febc2d54184f8fd83da25e4
Kelly Gowing <Gowing.kelly@yahoo.com>
2021-01-03 21:05:06 -0800

    technical skills: replace table with flexbox divs

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,21 +34,22 @@ const HomeIndex = () => {
           Full Stack Web Developer with a background in Marketing and Event Management. I'm a 2020 UCSD Full-Stack Web Development Bootcamp and 2019 SDSU Marketing graduate. My dream is to be a UI/UX developer or Product Manager.
           </p>
           <h3>Technical Skills:</h3>
-          <table>
-            <tbody>
-              <tr>
-                <td><i className="skills fab fa-3x fa-html5"><span className="tooltiptext">HTML</span></i></td>
-                <td><i className="skills fab fa-3x	fa-css3-alt"><span className="tooltiptext">CSS</span></i></td>
-                <td><i className="skills fab fa-3x	fa-react"><span className="tooltiptext">React</span></i></td>
-                <td><i className="skills fab fa-3x	fa-js-square"><span className="tooltiptext">JavaScript</span></i></td>
-                <td><i className="skills fab fa-3x	fa-node-js"><span className="tooltiptext">Node.js</span></i></td>
-                <td><i className="skills fab fa-3x	fa-git"><span className="tooltiptext">Git</span></i></td>
-                <td><i className="skills fab fa-3x	fa-shopify"><span className="tooltiptext">Liquid(Shopify)</span></i></td>
-                <td><i className="skills fab fa-3x	fa-bootstrap"><span className="tooltiptext">Bootstrap</span></i></td>
-                <td><i className="skills fas fa-3x fa-database"><span className="tooltiptext">SQL</span></i></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="skills-container">
+            <div className="skills-row">
+                <i className="skills fab fa-3x fa-html5"><span className="tooltiptext">HTML</span></i>
+                <i className="skills fab fa-3x	fa-css3-alt"><span className="tooltiptext">CSS</span></i>
+                <i className="skills fab fa-3x	fa-react"><span className="tooltiptext">React</span></i>
+                <i className="skills fab fa-3x	fa-js-square"><span className="tooltiptext">JavaScript</span></i>
+                <i className="skills fab fa-3x	fa-node-js"><span className="tooltiptext">Node.js</span></i>
+            </div>
+            <div className="skills-row">
+            <i className="skills fab fa-3x	fa-git"><span className="tooltiptext">Git</span></i>
+                <i className="skills fab fa-3x	fa-shopify"><span className="tooltiptext">Liquid(Shopify)</span></i>
+                <i className="skills fab fa-3x	fa-bootstrap"><span className="tooltiptext">Bootstrap</span></i>
+                <i className="skills fas fa-3x fa-database"><span className="tooltiptext">SQL</span></i>
+                <i className="skills fab fa-3x fa-sketch"><span className="tooltiptext">Sketch</span></i>
+            </div>
+          </div>
           <ul className="actions">
             <li>
               <a href="#worktogether" className="button">
