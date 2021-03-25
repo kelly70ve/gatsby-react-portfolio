38ee87d471d70ea246c0c6a1615c7856964227c6
Kelly Gowing <Gowing.kelly@yahoo.com>
2021-03-25 15:54:12 -0700

    **important** div#main needed for text to show

--- src/pages/full-portfolio.js
+++ src/pages/full-portfolio.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 // import Gallery from '../components/Gallery';
 import Layout from '../components/layout';
 // import Contact from '../components/Contact'
@@ -7,13 +6,11 @@ import Layout from '../components/layout';
 // import MyForm from '../components/MyForm';
 
 const fullPortfolio = () => {
-  const siteTitle = 'Kelly Gowing - Full-Stack Developer';
-  const siteDescription =
-    'Web Devlopment portfolio by Kelly Gowing, using Gatsby Starter - Strata';
-
   return (
     <Layout>
-      <Helmet />
+      <div id="main">
+        <h1>Full Portfolio</h1>
+      </div>
     </Layout>
   );
 };
