d4866f0f1f6c8d583e3b3c596e22046f4cbe9bac
Kelly Gowing <Gowing.kelly@yahoo.com>
2021-04-03 11:20:00 -0700

    add photography post template

---
+++ src/pages/full-portfolio/photography/Paris/index.md
@@ -0,0 +1,14 @@
+---
+path: '/paris'
+title: 'paris'
+featureImage: ./paris.jpg
+caption: 'Armonia'
+description: 'Chat and service app for contactless dining'
+deployedApp: 'https://cobramon.herokuapp.com/signup'
+github: 'https://github.com/goodbyetonto/cobramon'
+position: 0
+template: 'PhotoPost'
+postType: 'photography'
+---
+
+<img src="https://lh3.googleusercontent.com/2UFudnNLxIRL5-WbxijdEG6zscX0N4pmpKy49tGPsf20vW2u_h3ZYFrXP-g4d6bb9lm41lxSx0giCPyUbI5g_S1mh_p7cbVEOdHbQjc6kaEXoI6UanUlUcBSdVRd90TE4AGGSha6UuQ"/>
---
+++ src/templates/PhotoPost.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import Layout from '../components/layout';
+import Masonry from '../components/Masonry';
+import { graphql } from 'gatsby';
+
+export default function Template({ data }) {
+  const post = data.markdownRemark;
+  const style = {
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
+
+  return (
+    <Layout>
+      <div id="main">
+        <Masonry />
+      </div>
+    </Layout>
+  );
+}
+
+export const postQuery = graphql`
+  query PhotoPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+        description
+        deployedApp
+        github
+      }
+    }
+  }
+`;
