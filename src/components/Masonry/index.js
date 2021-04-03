67fd9f3360b3bac041557c1f4c7b4129e91bbc60
Kelly Gowing <Gowing.kelly@yahoo.com>
2021-04-03 11:14:35 -0700

    add image gallery masonry package

---
+++ src/components/Masonry/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
+import { photos } from './photos';
+
+/* popout the browser and maximize to see more columns! -> */
+export default class Masonry extends Component {
+  render() {
+    return <Gallery photos={photos} direction={'column'} />;
+  }
+}
